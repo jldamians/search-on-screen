edf56a325012587c4bee2647be2d000344855e51
Jose Damian jldamians <jlds161089@gmail.com>
2020-02-11 11:27:06 -0500

    [update] Modificando el ejemplo

--- example/index.js
+++ example/index.js
@@ -1,14 +1,19 @@
-const path = require("path");
-
+const path = require('path');
 const searching = require('../lib');
 
-
 (async() => {
+  const starting = new Date().getTime();
   const refPath = path.join(__dirname, "reference.PNG");
-
   const result = await searching(refPath);
+  const ending = new Date().getTime();
 
-  console.log(result);
+  if (!result.position) {
+    console.log('La imagen de referencia no fue encontrada');
+  } else {
+    console.log(`Imagen encontrada en la posición (${result.position.x}, ${result.position.y})`);
+  }
+
+  console.log(`Time: ${(ending - starting) / 1000} seconds`);
 })();
 
 
